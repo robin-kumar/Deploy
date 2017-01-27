792044cf23899e0242b87a13a4e50851ec609746
robin-kumar <kumar.r.robin@gmail.com>
2017-01-27 16:56:35 +0000

    Initial commit of the project files

---
+++ demo/src/main/java/Jcore/demo/App.java
@@ -0,0 +1,13 @@
+package Jcore.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
